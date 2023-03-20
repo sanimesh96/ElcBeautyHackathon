ab9334d86c08607f1ec3eca9d40bf69b44f64b9c
Sanimesh96 <sanimesh96@gmail.com>
2023-03-20 17:22:20 +0530

    Beauty AR Added

--- server/db.js
+++ server/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
-const uri = `mongodb://localhost:27017/SmartOdisha?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false`;
+// const uri = `mongodb://localhost:27017/SmartOdisha?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false`;
 //const uri = "mongodb+srv://sanimesh96:satwik123@cluster0.wt1rqpn.mongodb.net/SmartOdisha?retryWrites=true&w=majority";
+const uri = `mongodb+srv://Satwik:Newton%408730@cluster1.6w8tiit.mongodb.net/BeautyHackathon?retryWrites=true&w=majority`
 
 
 const connectToMongo = () => {
