341feeb4f3df10b092ed162714d8abce6722b99b
Alpana <alpana.nanda12@gmail.com>
2023-03-20 17:19:18 +0530

    logout

--- client/src/Components/LandingPage/TopSection/Navbar/Navbar.jsx
+++ client/src/Components/LandingPage/TopSection/Navbar/Navbar.jsx
@@ -3,11 +3,16 @@ import './navbar.css'
 import logo from '../img/log.png'
 import { NavLink } from 'react-router-dom';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faMagnifyingGlass , faUser , faCartShopping} from '@fortawesome/free-solid-svg-icons';
+import {faMagnifyingGlass , faSignOut , faCartShopping} from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 function Navbar() {
 
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        localStorage.removeItem("email");
+        navigate("/login");
+      };
     const cartitem = useSelector((store) => store.cart);
 
 
@@ -29,12 +34,11 @@ function Navbar() {
                                 <li><NavLink to='/'>Home</NavLink></li>
                                 <li><NavLink to='/shop'>Shop</NavLink></li>
                                 <li className="res_m"><NavLink to='/about'>About</NavLink></li>
-                                <li className="res_m"><NavLink to='/contact'>Contact</NavLink></li>
                             </ul>
                     </div>
                     <div className='navbar_icons'>
                     <NavLink className="res_m"><FontAwesomeIcon icon={faMagnifyingGlass} className="cart_logo"/></NavLink>
-                    <NavLink to='/profile'><FontAwesomeIcon icon={faUser} className="cart_logo"/></NavLink>
+                    <NavLink to='/profile'><FontAwesomeIcon icon={faSignOut} className="cart_logo" onClick={handleLogout}/></NavLink>
                     <NavLink to='/cart' className="res_m"><div className='cartLogo_div'>
                     <FontAwesomeIcon icon={faCartShopping} className="cart_logo"/>
                     {
